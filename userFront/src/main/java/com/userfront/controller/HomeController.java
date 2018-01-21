afebfba92094f4fd836e5adf16cc6a86ac5badf1
shha268@gmail.com <shha268@gmail.com>
2018-01-21 11:42:28 +1100

    Adding signup form
    Adding SignUp form to the project

--- userFront/src/main/java/com/userfront/controller/HomeController.java
+++ userFront/src/main/java/com/userfront/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.userfront.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.userfront.domain.User;
 
 @Controller
 public class HomeController {
@@ -14,4 +19,51 @@ public class HomeController {
 	public String index() {
 		return "index";
 	}
+	
+	//@RequestMapping(value="/signup" , method= RequestMethod.GET)
+	@GetMapping("/signup")
+	public String signup(Model model) {
+		
+		User user = new User();
+		model.addAttribute("user",user);
+		
+		return "signup";
+	}
+	
+	@PostMapping("/signup")
+	public String signup() {
+		
+		
+		return "";
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
