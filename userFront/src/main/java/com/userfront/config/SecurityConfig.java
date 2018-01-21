eaa4de418a41b6925995658e2dfe6cdc237954fb
shha268@gmail.com <shha268@gmail.com>
2018-01-21 20:21:00 +1100

    Authority Class - UserClass, Role Class,..
    create package com.userfront.domain.security
    Authority Class under the com.userfront.domain.security which implements GrantedAuthority
    Role Class which contains Roles name
    UserRole which connect user and its specified roles
    AddingUserSecurityService class
    Implementing User class Form UserDetails

--- userFront/src/main/java/com/userfront/config/SecurityConfig.java
+++ userFront/src/main/java/com/userfront/config/SecurityConfig.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.userfront.service.UserServiceImpl.UserSecurityService;
 
 @Configuration
 @EnableWebSecurity
@@ -22,7 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserSecurityService userSercurityService;
 	
-	//use this parameter to encode my psswords :D
+	//use this parameter to encode my passwords :D
 	private static final String Salt = "NissanMaxima2004:D";
 	
 	@Bean
@@ -42,7 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			"/error/**",
 			"/console/**",
 			"/signup",
-	}
+	};
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -61,6 +64,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 			.rememberMe();
 		
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.userDetailsService(userSercurityService).passwordEncoder(passwordEncoder());
+
 	}
 }
 
