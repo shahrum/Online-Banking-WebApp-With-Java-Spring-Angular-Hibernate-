1555ac717c72d8a6f2a0daa2229e7db169903b0d
shha268@gmail.com <shha268@gmail.com>
2018-01-22 19:31:32 +1100

    primaryAccount.html
    completeing primaryAccount.html and also updating the Accountcontroller

--- userFront/src/main/java/com/userfront/controller/AccountController.java
+++ userFront/src/main/java/com/userfront/controller/AccountController.java
@@ -1,14 +1,27 @@
 package com.userfront.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.userfront.domain.PrimaryAccount;
+import com.userfront.domain.User;
+import com.userfront.service.UserService;
 
 @Controller
 @RequestMapping("/account")
 public class AccountController {
+	@Autowired
+	private UserService userService;
 
 	@RequestMapping("/primaryAccount")
-	public String primaryAccount() {
+	public String primaryAccount(Model model, Principal principal) {
+		User user = userService.findByUsername(principal.getName());
+		PrimaryAccount primaryAccount = user.getPrimaryAccount();
+		model.addAttribute("primaryAccount", primaryAccount);
 		return "primaryAccount";
 	}
 	
