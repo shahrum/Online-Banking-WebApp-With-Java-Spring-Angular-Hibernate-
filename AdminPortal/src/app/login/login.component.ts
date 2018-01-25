3e40014a799f1579a2f241e63190d0082b8cdebb
shha268@gmail.com <shha268@gmail.com>
2018-01-25 19:24:03 +1100

    Login
    adding some updates to login component and also creating login service

--- AdminPortal/src/app/login/login.component.ts
+++ AdminPortal/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+	loggedIn : boolean;
+	username :string;
+	password : string;
 
-  constructor() { }
+  constructor(private _loginService:LoginService) { 
+  	//local storage is sth like cookie but much more better :D
+  	if (localStorage.getItem('PortalAdminHasLoggedIn') == '') { 
+  		this.loggedIn = false;
+  	} else {
+  		this.loggedIn = true;
+  	}
+  }
 
   ngOnInit() {
   }
