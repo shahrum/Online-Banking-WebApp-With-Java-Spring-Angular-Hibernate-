9a6fa79a6cdef3b40ad293614e1c38af757f4f47
shha268@gmail.com <shha268@gmail.com>
2018-01-27 13:57:23 +1100

    recap

--- AdminPortal/src/app/user-account/user-account.component.ts
+++ AdminPortal/src/app/user-account/user-account.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { UserService } from '../services/user.service';
+import {UserService} from '../services/user.service';
 
 @Component({
   selector: 'app-user-account',
@@ -9,38 +9,38 @@ import { UserService } from '../services/user.service';
 })
 export class UserAccountComponent implements OnInit {
 
-    userList: Object[];
-  
-  constructor(private userService: UserService, private router: Router) {
-    this.getUsers();
-  }
+  	userList: Object[];
+	
+	constructor(private userService: UserService, private router: Router) {
+		this.getUsers();
+	}
 
-  getUsers() {
-    this.userService.getUsers().subscribe(
-      res => {
-            this.userList = JSON.parse(JSON.parse(JSON.stringify(res))._body);
-          },
-          error => console.log(error)
-    )
-  }
+	getUsers() {
+		this.userService.getUsers().subscribe(
+			res => {
+        		this.userList = JSON.parse(JSON.parse(JSON.stringify(res))._body);
+      		},
+      		error => console.log(error)
+		)
+	}
 
-  onSelectPrimary(username: string) {
-    this.router.navigate(['/primaryTransaction', username]);
-  }  
+	onSelectPrimary(username: string) {
+    	this.router.navigate(['/primaryTransaction', username]);
+  	}	
 
-  onSelectSavings(username: string) {
-    this.router.navigate(['/savingsTransaction', username]);
-  }  
+  	onSelectSavings(username: string) {
+    	this.router.navigate(['/savingsTransaction', username]);
+  	}	
 
-  enableUser(username: string) {
-    this.userService.enableUser(username).subscribe();
-    location.reload();
-  }
+  	enableUser(username: string) {
+  		this.userService.enableUser(username).subscribe();
+  		location.reload();
+  	}
 
-  disableUser(username: string) {
-    this.userService.disableUser(username).subscribe();
-    location.reload();
-  }
+  	disableUser(username: string) {
+  		this.userService.disableUser(username).subscribe();
+  		location.reload();
+  	}
 
 
   ngOnInit() {
