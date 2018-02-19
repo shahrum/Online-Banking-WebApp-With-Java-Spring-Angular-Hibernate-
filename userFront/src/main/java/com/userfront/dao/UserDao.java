14c4a6ae9b4f84d5fc38b7fcba9363a527a7e9c4
shha268@gmail.com <shha268@gmail.com>
2018-02-19 13:20:31 +1100

    -
    -

--- userFront/src/main/java/com/userfront/dao/UserDao.java
+++ userFront/src/main/java/com/userfront/dao/UserDao.java
@@ -9,5 +9,5 @@ import com.userfront.domain.User;
 public interface UserDao extends CrudRepository<User, Long> {
 	User findByUsername(String username);
     User findByEmail(String email);
-    List<User> findAll();
+    List<User> findAll(); 
 }
